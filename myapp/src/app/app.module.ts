d8581b39ce48852f919c3d278a47947e8bbff1a4
Padmakumar18 <padmakumar41759@gmail.com>
2025-04-14 02:47:45 +0530

    Code updated

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -1,27 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductDetailComponent } from './components/product-detail/product-detail.component';
-import { CategoryFilterPipe } from './pipes/category-filter.pipe';
-import { SortPipe } from './pipes/sort.pipe';
-import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductListComponent,
-    ProductDetailComponent,
-    CategoryFilterPipe,
-    SortPipe
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
-  providers: [ProductService],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
