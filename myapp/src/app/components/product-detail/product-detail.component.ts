8da95903cb2f9f05c5c606c9eca11437401908e0
Padmakumar18 <padmakumar41759@gmail.com>
2025-04-14 02:46:13 +0530

    Code updated

--- myapp/src/app/components/product-detail/product-detail.component.ts
+++ myapp/src/app/components/product-detail/product-detail.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-product-detail',
+//   imports: [],
+//   templateUrl: './product-detail.component.html',
+//   styleUrl: './product-detail.component.css'
+// })
+// export class ProductDetailComponent {
+
+// }
+
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
   templateUrl: './product-detail.component.html',
-  styleUrl: './product-detail.component.css'
+  styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent {
-
+  @Input() product: any;
 }
--- myapp/src/app/components/product-list/product-list.component.ts
+++ myapp/src/app/components/product-list/product-list.component.ts
@@ -1,11 +1,28 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-product-list',
+//   imports: [],
+//   templateUrl: './product-list.component.html',
+//   styleUrl: './product-list.component.css'
+// })
+// export class ProductListComponent {
+
+// }
+
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  products = this.productService.getProducts();
+  selectedCategory: string = '';
+  selectedOption: string = '';
+  categories = this.productService.getCategories();
 
+  constructor(private productService: ProductService) {}
 }
